025ea876750731344682608e536a8f7f60ce75b4
Shuta Morimoto <shuta3mori@gmail.com>
2026-05-17 09:03:06 +0900

    abc458-d modify

--- test/abc458_20260516/d.cpp
+++ test/abc458_20260516/d.cpp
@@ -7,17 +7,41 @@ int main()
 {
     ll x, q;
     std::cin >> x >> q;
-    std::list<ll> list;
-    list.push_back(x);
-    ll m = x;
-    ll pos = 0;
+    std::priority_queue<ll> ql;
+    std::priority_queue<ll, std::vector<ll>, std::greater<ll>> qr;
     for (int i = 0; i < q; i++)
     {
-        ll a, b, next;
+        ll a, b;
         std::cin >> a >> b;
-        list.insert(std::lower_bound(list.begin(), list.end(), a), a);
-        list.insert(std::lower_bound(list.begin(), list.end(), b), b);
-        std::cout << *(std::next(list.cbegin(), list.size() / 2)) << "\n";
+        if (a<x)
+        {
+            ql.push(a);
+        }
+        else
+        {
+            qr.push(a);
+        }
+        if (b<x)
+        {
+            ql.push(b);
+        }
+        else
+        {
+            qr.push(b);
+        }
+        if (ql.size() < qr.size())
+        {
+            ql.push(x);
+            x = qr.top();
+            qr.pop();
+        }
+        else if (ql.size() > qr.size())
+        {
+            qr.push(x);
+            x = ql.top();
+            ql.pop();
+        }
+        std::cout << x << "\n";
     }
     return 0;
 }
